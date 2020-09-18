28783dba6de2e4c3b3420018a98bde5070f8ce6e
stepin104458 <69188910+stepin104458@users.noreply.github.com>
2020-09-18 23:09:26 +0530

    Create Fs.h

---
+++ FS/inc/Fs.h
@@ -0,0 +1,27 @@
+#ifndef __Food_order_H__
+#define __Food_order_H__
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+void returnfunc(void);
+
+void mainmenu(void);
+
+void administration(void);
+void customer(void);
+
+void addrecord(void);
+void deleterecord(void);
+void searchrecord(void);
+void viewrecord(void);
+
+void vegetarian(void);
+void nonvegetarian(void);
+void details(void);
+void aboutus(void);
+
+//void Password(void);
+int getdata();
+#endif
